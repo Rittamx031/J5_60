3f53b25840dc673174e4e48648dd66f62e8898d4
Rittam <thatv12122003@gmail.com>
2023-07-11 13:28:40 +0700

    enity ve and vekey

--- src/main/java/j5_60/cinematicket/cinematicket/entity/Ve.java
+++ src/main/java/j5_60/cinematicket/cinematicket/entity/Ve.java
@@ -1,5 +1,44 @@
 package j5_60.cinematicket.cinematicket.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+
 public class Ve {
-    
+    @EmbeddedId
+    private VeKey id;
+    @ManyToOne
+    @MapsId("id_lich_chieu")
+    @JoinColumn(name = "id_lich_chieu")
+    private LichChieu lichChieu;
+    @ManyToOne
+    @MapsId("id_ghe")
+    @JoinColumn(name = "id_ghe")
+    private Ghe ghe;
+    @Column(name = "gia")
+    private double gia;
+    @Column(name = "trang_thai")
+    private int trangThai;
+    @Column(name="ngay_dat_ve")
+    private LocalDate ngayDatVe;
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
+    @Column(name = "update_by")
+    private UUID updateBy;
+    @Column(name = "create_by")
+    private UUID createBy;
+    @Column(name = "deleted")
+    private boolean deleted;
+    @ManyToOne
+    @JoinColumn(name="id_hoa_don")
+    private HoaDon hoaDon;
+
 }
---
+++ src/main/java/j5_60/cinematicket/cinematicket/entity/key/VeKey.java
@@ -0,0 +1,42 @@
+package j5_60.cinematicket.cinematicket.entity.key;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * VeKey
+ */
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VeKey {
+    UUID id_lich_chieu;
+
+    @Column(name = "id_ghe")
+    UUID id_ghe;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof VeKey))
+            return false;
+        VeKey other = (VeKey) obj;
+        return Objects.equals(id_lich_chieu, other.id_lich_chieu) && Objects.equals(id_ghe, other.id_ghe);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_lich_chieu, id_ghe);
+    }
+
+}
